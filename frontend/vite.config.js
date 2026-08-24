b37315b730f959ed6061cb415f8ef90f6483fdef
teyungromio <teyungromio@gmail.com>
2026-08-24 13:09:09 +0545

    Fix vite config

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,8 +1,9 @@
 import { defineConfig } from "vite";
-import react from "To-Do-List";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
+  base: "/To-Do-List/",
   server: {
     port: 5173
   }
